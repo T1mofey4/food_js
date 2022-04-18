a2b4cb6e00f7070e945cfafce62661e5e0828bb9
Timofey <timofeyvalerievich@gmail.com>
2022-04-19 00:04:24 +0300

    added reset form and delete notification about sending user information. chenged format data to json

--- dist/js/script.js
+++ dist/js/script.js
@@ -214,15 +214,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 const request = new XMLHttpRequest();
                 request.open('POST', '/server.php');
 
-                // request.setRequestHeader('Content-type', 'multipart/form-data');
+                request.setRequestHeader('Content-type', 'application/json');
                 const formData = new FormData(form);
 
-                request.send(formData);
+                const object ={};
+                formData.forEach(function(value, key){
+                    object[key] = value;
+                });
+                const json = JSON.stringify(object);
+                
+                request.send(json);
 
                 request.addEventListener('load', () => {
                     if (request.status === 200){
                         statusMessage.textContent = message.success;
                         console.log(request.response);
+                        form.reset();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 2000);
                     } else {
                         statusMessage.textContent = message.failure;
                     }
