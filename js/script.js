45e7d0d4f6553d9f6879dd252dd3c11a8c134a43
Timofey <timofeyvalerievich@gmail.com>
2022-04-25 18:41:52 +0300

    added moving slides by click on dots and repeated code moving to function dotsMove();

--- js/script.js
+++ js/script.js
@@ -314,6 +314,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //слайдер
 
     const  slides = document.querySelectorAll('.offer__slide'),
+           slider = document.querySelector('.offer__slider'),
            prev = document.querySelector('.offer__slider-prev'),
            next = document.querySelector('.offer__slider-next'),
            total = document.querySelector('#total'),
@@ -343,6 +344,59 @@ document.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for(let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0){
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    //перемещение dots вслед за слайдером
+
+    function dotsMove() {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+
+     
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
@@ -362,6 +416,8 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dotsMove();
     });
 
     prev.addEventListener('click', () => {
@@ -383,6 +439,29 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dotsMove();
+    });
+
+    //перемещение по клику на dots
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+          const slideTo = e.target.getAttribute('data-slide-to');
+          
+          slideIndex = slideTo;
+          offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+          slidesField.style.transform = `translate(-${offset}px)`;
+
+          if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+          } else {
+            current.textContent = slideIndex;
+          }
+          
+          dotsMove();        
+        });
     });
 
     // showSlides(slideIndex);
